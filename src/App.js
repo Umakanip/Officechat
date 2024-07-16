3e4880689c594aba22177beb64d2e915af1a0be6
Abishck <abishckpcs02@gmail.com>
2024-07-16 15:28:34 +0530

    login page image

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import Header from './components/chatpage/header.tsx'
 import Sidemenu from './components/chatpage/sidemenu.tsx'
-import LoginForm from './components/LoginForm/LoginForm';
 function App() {
   return (
     <div className="App">
