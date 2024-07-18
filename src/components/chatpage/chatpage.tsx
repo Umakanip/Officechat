dd783220b8bf7de670135a7e852e7661bbae6c30
Umakani <umakani310@gmail.com>
2024-07-18 12:20:28 +0530

    Login and chat page merging

---
+++ src/components/chatpage/chatpage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Header from '../chatpage/header.tsx'
+import Sidemenu from '../chatpage/sidemenu.tsx'
+const Chatpage: React.FC = () => {
+return(<>
+
+<div>
+    <Header />
+    <Sidemenu/>
+
+        </div></>);
+}
+export default Chatpage;
